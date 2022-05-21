b1bdd60cab32a4384a3a85a2059fdda64768bf7a
Thibaut Gautier <shanyujuneja@gmail.com>
2022-05-21 14:15:38 +0200

    Added new loop test

--- src/main/java/dev/sim0n/evaluator/manager/TestManager.java
+++ src/main/java/dev/sim0n/evaluator/manager/TestManager.java
@@ -3,8 +3,8 @@ package dev.sim0n.evaluator.manager;
 import dev.sim0n.evaluator.Main;
 import dev.sim0n.evaluator.test.Test;
 import dev.sim0n.evaluator.test.impl.annotation.AnnotationTest;
-import dev.sim0n.evaluator.test.impl.exception.OpaqueConditionTest;
-import lombok.Getter;
+import dev.sim0n.evaluator.test.impl.flow.OpaqueConditionTest;
+import dev.sim0n.evaluator.test.impl.flow.WeirdLoopTest;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,7 +13,8 @@ import java.util.List;
 public class TestManager {
     private static final List<Class<? extends Test>> TEST_CLASSES = Arrays.asList(
             AnnotationTest.class,
-            OpaqueConditionTest.class
+            OpaqueConditionTest.class,
+            WeirdLoopTest.class
     );
 
     private final List<Test> tests = new ArrayList<>();
